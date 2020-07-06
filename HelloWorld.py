b3e963e3b1023ae9948d18f7d59e56d29e9755a4
aldenhart <alden08@harts.org>
2020-07-06 11:35:34 -0400

    Mucking about with hello world

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,16 @@
-from smbus2 import SMBus
+import qwiic_i2c
+from smbus2 import SMBus, i2c_msg
 
-# Open i2c bus 1 and read one byte from address 80, offset 0
+# Open i2c bus 1 and read one byte from address 0x77, offset 0
 bus = SMBus(1)
-b = bus.read_byte_data(77, 0)
-print(b)
+addrs = [0x74, 0x77]
+try:
+    for addr in addrs:
+        print("Addr: {:}".format(addr))
+        for i in range(7):
+            b = bus.read_byte_data(addr, i)
+            print("Read back {:}".format(b))
+except IOError as err:
+    print("Failed to connect to {:} err {:}".format(addr, err))
+
 bus.close()
